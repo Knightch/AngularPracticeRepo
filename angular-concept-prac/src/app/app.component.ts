e54e3191e3d1a3ce1243fb18b6c1f69d1079ca9d
knightch <amankumarpandey2397@gmail.com>
2022-07-05 12:05:13 +0530

    using  to fetch the data

--- angular-concept-prac/src/app/app.component.ts
+++ angular-concept-prac/src/app/app.component.ts
@@ -7,8 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   clickStatus = "you does not click button yet";
+  inputValue = '';
+  inputAnotherValue = '';
 
   onClickStatusChanage() {
     this.clickStatus = "You click button just!"
+  }
+
+  onTakingInput(event: Event) {
+    // console.log(event)
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onTakingAnotherWayOfInput(event: any) {
+    this.inputAnotherValue = event.target.value;
   }
 }
