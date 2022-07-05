160c1d42117a93fde43daaef8f2df068146b3ab2
knightch <amankumarpandey2397@gmail.com>
2022-07-05 12:34:21 +0530

    combining property binding and two way binding

--- angular-concept-prac/src/app/app.component.ts
+++ angular-concept-prac/src/app/app.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  clickStatus = "you does not click button yet";
+  allowButton = false;
+  inputStatus = "Here is your Input Value";
   inputValue = '';
   // inputAnotherValue = '';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowButton = true;
+    }, 5000)
+  }
+
   onClickStatusChanage() {
-    this.clickStatus = "You click button just!"
+    this.inputStatus = this.inputValue;
   }
 
   onTakingInput(event: Event) {
