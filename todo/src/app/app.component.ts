61601cf39ebc8b51db16a411154f4bf4c5f5fd48
knightch <amankumarpandey2397@gmail.com>
2022-07-09 00:01:59 +0530

    initial set up for to do list

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todo';
+  private list = new TodoList("Aman", [
+    new TodoItem("Go for run", true),
+    new TodoItem("get flowers"),
+    new TodoItem("collect tickets")
+  ]);
+
+  get userName(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length
+  }
+}
