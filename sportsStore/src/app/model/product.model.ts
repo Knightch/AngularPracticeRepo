8ed702605fadb34c0c7845a81a1a75827363cbd0
knightch <amankumarpandey2397@gmail.com>
2022-07-09 16:14:40 +0530

    creating product class and dummy data source

---
+++ sportsStore/src/app/model/product.model.ts
@@ -0,0 +1,9 @@
+// creating model class
+export class Product {
+  constructor(
+    public id?: number,
+    public name?: string,
+    public category?: string,
+    public description?: string,
+    public price?: number) { }
+}
