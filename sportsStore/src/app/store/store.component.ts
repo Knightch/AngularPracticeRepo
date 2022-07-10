012fd31a1859583e9480f23b830d7edc350f243a
knightch <amankumarpandey2397@gmail.com>
2022-07-10 12:19:21 +0530

    Adding Product Pagination

--- sportsStore/src/app/store/store.component.ts
+++ sportsStore/src/app/store/store.component.ts
@@ -8,15 +8,21 @@ import { ProductRepository } from '../model/product.repository';
   styleUrls: ['./store.component.scss']
 })
 export class StoreComponent implements OnInit {
-
+  // adding category selection
+  selectedCategory: string | undefined;
+  
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
   }
   ngOnInit(): void {
   }
