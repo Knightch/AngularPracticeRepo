601db3bf20b95bdf0060565b5a61bd144a4bf376
knightch <amankumarpandey2397@gmail.com>
2022-07-11 13:35:29 +0530

    styling pagination

--- sportsStore/src/app/store/store.component.ts
+++ sportsStore/src/app/store/store.component.ts
@@ -10,15 +10,34 @@ import { ProductRepository } from '../model/product.repository';
 export class StoreComponent implements OnInit {
   // adding category selection
   selectedCategory: string | undefined;
-  
+  // Adding Product Pagination
+  productsPerPage = 4;
+  selectedPage = 1;
+
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+  get pageNumbers(): number[] {
+    return Array(Math.ceil(this.repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
   }
 
   changeCategory(newCategory?: string) {
