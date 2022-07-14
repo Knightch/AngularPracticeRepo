45219f8010638742752cf55bea8e6911c31dcd77
knightch <amankumarpandey2397@gmail.com>
2022-07-14 13:34:00 +0530

    using ngx-cookie-service

--- UsingCookiesAngular/src/app/app.component.ts
+++ UsingCookiesAngular/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'UsingCookiesAngular';
+  cookie_name = '';
+  all_cookies: any = '';
+
+  constructor(private cookieService: CookieService) { }
+
+  setCookie() {
+    this.cookieService.set('name', 'Aman Pandey');
+  }
+
+  deleteCookie() {
+    this.cookieService.delete('name');
+  }
+
+  deleteAll() {
+    this.cookieService.deleteAll();
+  }
+
+  ngOnInit(): void {
+    this.cookie_name = this.cookieService.get('name');
+    this.all_cookies = this.cookieService.getAll();
+  }
 }
