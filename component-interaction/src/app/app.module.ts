6d7a3dcd50f18964caa132d0cc5e7da233a0f7ee
knightch <amankumarpandey2397@gmail.com>
2022-07-22 12:11:42 +0530

    intercept input property changes with setter

--- component-interaction/src/app/app.module.ts
+++ component-interaction/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroChildComponentComponent } from './hero-child-component/hero-child-component.component';
 import { HeroParentComponentComponent } from './hero-parent-component/hero-parent-component.component';
+import { NameChildComponent } from './name-child/name-child.component';
+import { NameParentComponent } from './name-parent/name-parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroChildComponentComponent,
-    HeroParentComponentComponent
+    HeroParentComponentComponent,
+    NameChildComponent,
+    NameParentComponent
   ],
   imports: [
     BrowserModule
